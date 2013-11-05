59158aaf4b715f83c0aa3747959bc93dc6894510
hauptmech <hauptmech@gmail.com>
2013-11-05 23:22:24 +0100

    Fix demo

--- src/eigen_initializer_list_demo.cpp
+++ src/eigen_initializer_list_demo.cpp
@@ -5,12 +5,22 @@ using namespace Eigen;
 
 int main()
 {
-Matrix3d aa {1.2,2.2,3.3, 4.2,2.5,6.3, 7.2,8.2,9.3};
-Matrix3d bb {{1.2,2.2,3.3},{4.2,2.5,6.3},{7.2,8.2,9.3}};
-//Matrix3d m = Matrix3d::Random();
-//Matrix2d m {{1.2,2.2},{3.2,4.5}};
-//MatrixXd m {1.2,2.2,3.3, 4.2,2.5,6.3, 7.2,8.2,9.3};
-Vector3d m  {1,3,3};
-std::cout << m << std::endl;
+    // Init a fixed size 3x3 matrix
+    Matrix3d m3 {{1.2,2.2,3.3},{4.2,2.5,6.3},{7.2,8.2,9.3}};
+    std::cout << "\nm3:\n" << m3 << std::endl;
+
+
+    // Initialize a variable length Matrix to 2x2
+    MatrixXd mY {{1.2,2.2},{3.2,4.5}};
+    std::cout << "\nmY:\n" << mY << std::endl;
+
+    // Initialize a variable length Matrix (Vector) to 9x1
+    // 2D Matrices are initialized column-wise
+    MatrixXd mX {1.2,2.2,3.3, 4.2,2.5,6.3, 7.2,8.2,9.3};
+    std::cout << "\nmX:\n" << mX << std::endl;
+
+    // Init a 3 element vector
+    Vector3d v3  {1,3,3};
+    std::cout << "\nv3:\n" << v3 << std::endl;
 }
 
